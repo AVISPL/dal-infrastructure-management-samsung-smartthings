eb24b246485532a5a931993d15581939d2967d0c
ThanhD-AVISPL <Thanh.Do@avispl.com>
2022-08-01 17:31:48 +0700

    This commit is used to test Draft Adapter

--- src/test/java/com/avispl/symphony/dal/infrastructure/management/samsung/smartthings/SamSungSmartThingsAggregatorCommunicatorTest.java
+++ src/test/java/com/avispl/symphony/dal/infrastructure/management/samsung/smartthings/SamSungSmartThingsAggregatorCommunicatorTest.java
@@ -18,23 +18,18 @@ import org.junit.jupiter.api.Test;
  */
 public class SamSungSmartThingsAggregatorCommunicatorTest {
 	static SamSungSmartThingsAggregatorCommunicator samSungSmartThingsAggregatorCommunicator;
-	private static final int HTTP_PORT = 8088;
-	private static final int HTTPS_PORT = 8443;
 	private static final String HOST_NAME = "api.smartthings.com";
-	private static final String PROTOCOL = "https";
 
 	@BeforeEach
 	public void init() throws Exception {
 		samSungSmartThingsAggregatorCommunicator = new SamSungSmartThingsAggregatorCommunicator();
 		samSungSmartThingsAggregatorCommunicator.setTrustAllCertificates(false);
-		samSungSmartThingsAggregatorCommunicator.setProtocol(PROTOCOL);
 		samSungSmartThingsAggregatorCommunicator.setPort(443);
 		samSungSmartThingsAggregatorCommunicator.setHost(HOST_NAME);
 		samSungSmartThingsAggregatorCommunicator.setContentType("application/json");
 		samSungSmartThingsAggregatorCommunicator.setPassword("***REMOVED***");
 		samSungSmartThingsAggregatorCommunicator.init();
 		samSungSmartThingsAggregatorCommunicator.authenticate();
-		samSungSmartThingsAggregatorCommunicator.ping();
 	}
 
 	@AfterEach
